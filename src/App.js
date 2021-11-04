f0d1a41ba3b2ebc9985301fd532a2e1d5b5aa5fa
GaussYuan191 <1915059302@qq.com>
2021-11-04 23:16:08 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Item from './components/Item/Item'
+import Footer from './components/Footer/Footer'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <List>
+         <Item></Item>
+      </List>
+      <Footer></Footer>
     </div>
+
   );
 }
 
---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Item extends Component {
+  render () {
+    return (
+      <div>
+         元素
+      </div>
+    )
+  }
+}
+
+export default Item
